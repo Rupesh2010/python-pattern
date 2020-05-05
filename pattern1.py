fe3b6533d88b89391e7b8b5253cf77f37b663d83
Rupesh2010 <rupesh.phuyal1986@gmail.com>
2020-05-05 12:28:04 -0400

    Create pattern1.py

---
+++ pattern1.py
@@ -0,0 +1,13 @@
+num = 1
+for i in range(0, 3):
+    for j in range(0, 3):
+        print(num, end=" ")
+        num = num + 1
+    print("\r")
+
+num = 0
+for i in range(0, 3):
+    for j in range(0, 3):
+        print(num, end=" ")
+        num = num + 1
+    print("\r")
